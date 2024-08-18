d0abf86575740452d592b8176e227ddadc1515fa
Vinicius <vinicius.devjvm@gmail.com>
2024-08-18 14:55:29 -0300

    :lipstick: feat: add create opening request

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,12 +1,39 @@
 package handler
 
 import (
+	"github.com/ViniciusDSLima/streamming-golang/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "POST Openiing",
-	})
+
+	request := CreateOpeningRequest{}
+
+	c.BindJSON(&request)
+
+	err := request.Validate()
+
+	if err != nil {
+		logger.Errf("Validation Error: %v", err.Error())
+		sendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Link:     request.Link,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errf("Error creating opening: %v", err.Error())
+		sendError(c, http.StatusInternalServerError, "Error creating opening on database")
+		return
+	}
+
+	sendSuccess(c, "create-opening", &opening)
 }
