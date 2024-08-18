c4c41b3a0cee490353d8ceb9c83a22fdaa5ae664
Vinicius <vinicius.devjvm@gmail.com>
2024-08-18 13:56:37 -0300

    :lipstick: feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/ViniciusDSLima/streamming-golang/router"
+import (
+	"github.com/ViniciusDSLima/streamming-golang/config"
+	"github.com/ViniciusDSLima/streamming-golang/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errf("Config initialization failed: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
