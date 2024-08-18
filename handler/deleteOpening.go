28ddacc43ea5113f065b0d7e60a134d16b9d6c31
Vinicius <vinicius.devjvm@gmail.com>
2024-08-18 15:00:48 -0300

    :lipstick: feat: add delete opening request

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,12 +1,31 @@
 package handler
 
 import (
+	"fmt"
+	"github.com/ViniciusDSLima/streamming-golang/schemas"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func DeleteOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "DELETE Openiing",
-	})
+	id := c.Query("id")
+
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, fmt.Sprintf("opening %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, fmt.Sprintf("error deliting Opening with id: %s", id))
+		return
+	}
+
+	sendSuccess(c, "delete+opening", &opening)
 }
