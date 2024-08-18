d802584d6b0eafb8a873bfe9313b0b1ab8c955ab
Vinicius <vinicius.devjvm@gmail.com>
2024-08-18 14:20:53 -0300

    :lipstick: feat: add database handler

---
+++ config/sqlite.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"github.com/ViniciusDSLima/streamming-golang/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"os"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+
+	dbPath := "./db/main.db"
+
+	_, err := os.Stat(dbPath)
+
+	if os.IsNotExist(err) {
+		logger.Info("Database file not found, creating...")
+
+		err = os.MkdirAll("./db", os.ModePerm)
+
+		if err != nil {
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+
+		if err != nil {
+			return nil, err
+		}
+
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+
+	if err != nil {
+		logger.Errf("sqlite opening error: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Opening{})
+
+	if err != nil {
+		logger.Errf("sqlite automigration error: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
